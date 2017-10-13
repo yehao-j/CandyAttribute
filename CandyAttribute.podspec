665915e1db0cdcc90118cce7e51087bd8b4040a8
J-yezi <yehao1020@gmail.com>
2017-10-13 13:42:43 +0800

    init

---
+++ CandyAttribute.podspec
@@ -0,0 +1,16 @@
+Pod::Spec.new do |s|
+
+    s.name         = 'CandyAttribute'
+    s.version      = '0.0.1'
+    s.summary      = '富文本字典'
+    s.homepage     = 'https://github.com/J-yezi/CandyAttribute'
+    s.license      = { :type => 'MIT', :file => 'LICENSE' }
+    s.author       = { "J-yezi" => 'yehao1020@gmail.com' }
+    s.source       = { :git => 'https://github.com/J-yezi/CandyAttribute.git', :tag => s.version }
+
+    s.ios.deployment_target = '8.0'
+
+    s.source_files = 'Sources/*.swift'
+    s.dependency 'SwiftyAttributes', '4.1.0'
+
+end
---
+++ Sources/CandyAttribute.swift
@@ -0,0 +1,198 @@
+//
+//  CandyAttribute.swift
+//  demo
+//
+//  Created by jesse on 2017/10/13.
+//  Copyright © 2017年 jesse. All rights reserved.
+//
+
+import SwiftyAttributes
+
+struct CandyAttribute {}
+
+extension CandyAttribute {
+    static func dictionary(from attributes: [Attribute]) -> [StringKey: Any] {
+        var dict = [StringKey: Any]()
+        for attr in attributes {
+            dict[attr.keyName] = attr.foundationValue
+        }
+        return dict
+    }
+
+    public static func withTextColor(_ color: Color) -> [StringKey: Any] {
+        return dictionary(from: [Attribute.textColor(color)])
+    }
+
+    public static func withFont(_ font: Font) -> [StringKey: Any] {
+        return dictionary(from: [Attribute.font(font)])
+    }
+
+    public static func withParagraphStyle(_ style: ParagraphStyle) -> [StringKey: Any] {
+        return dictionary(from: [Attribute.paragraphStyle(style)])
+    }
+
+    public static func withBackgroundColor(_ color: Color) -> [StringKey: Any] {
+        return dictionary(from: [Attribute.backgroundColor(color)])
+    }
+
+    public static func withLigatures(_ ligatures: Ligatures) -> [StringKey: Any] {
+        return dictionary(from: [Attribute.ligatures(ligatures)])
+    }
+
+    public static func withKern(_ kernValue: Double) -> [StringKey: Any] {
+        return dictionary(from: [Attribute.kern(kernValue)])
+    }
+
+    public static func withStrikethroughStyle(_ style: StrikethroughStyle) -> [StringKey: Any] {
+        return dictionary(from: [Attribute.strikethroughStyle(style)])
+    }
+
+    public static func withUnderlineStyle(_ style: UnderlineStyle) -> [StringKey: Any] {
+        return dictionary(from: [Attribute.underlineStyle(style)])
+    }
+
+    public static func withStrokeColor(_ color: Color) -> [StringKey: Any] {
+        return dictionary(from: [Attribute.strokeColor(color)])
+    }
+
+    public static func withStrokeWidth(_ width: Double) -> [StringKey: Any] {
+        return dictionary(from: [Attribute.strokeWidth(width)])
+    }
+
+    public static func withTextEffect(_ effect: TextEffect) -> [StringKey: Any] {
+        return dictionary(from: [Attribute.textEffect(effect)])
+    }
+
+    public static func withBaselineOffset(_ offset: Double) -> [StringKey: Any] {
+        return dictionary(from: [Attribute.baselineOffset(offset)])
+    }
+
+    public static func withUnderlineColor(_ color: Color) -> [StringKey: Any] {
+        return dictionary(from: [Attribute.underlineColor(color)])
+    }
+
+    public static func withStrikethroughColor(_ color: Color) -> [StringKey: Any] {
+        return dictionary(from: [Attribute.strikethroughColor(color)])
+    }
+
+    public static func withObliqueness(_ obliquenessValue: Double) -> [StringKey: Any] {
+        return dictionary(from: [Attribute.obliqueness(obliquenessValue)])
+    }
+
+    public static func withExpansion(_ expansion: Double) -> [StringKey: Any] {
+        return dictionary(from: [Attribute.expansion(expansion)])
+    }
+
+    public static func withWritingDirections(_ directions: [WritingDirection]) -> [StringKey: Any] {
+        return dictionary(from: [Attribute.writingDirections(directions)])
+    }
+
+    public static func withVerticalGlyphForm(_ form: VerticalGlyphForm) -> [StringKey: Any] {
+        return dictionary(from: [Attribute.verticalGlyphForm(form)])
+    }
+
+    public static func withShadow(_ shadow: Shadow) -> [StringKey: Any] {
+        return dictionary(from: [Attribute.shadow(shadow)])
+    }
+
+    public static func withAttributes(_ attributes: [Attribute]) -> [String: Any] {
+        return dictionary(from: attributes)
+    }
+
+    public static func withAttribute(_ attribute: Attribute) -> [String: Any] {
+        return dictionary(from: [attribute])
+    }
+}
+
+extension Dictionary where Key == StringKey {
+    public static func +(lhs: [Key: Value], rhs: [Key: Value]) -> [Key: Value] {
+        var result = lhs
+        rhs.forEach{ result[$0] = $1 }
+        return result
+    }
+
+    public func withTextColor(_ color: Color) -> [StringKey: Any] {
+        return self + CandyAttribute.dictionary(from: [Attribute.textColor(color)])
+    }
+
+    public func withFont(_ font: Font) -> [StringKey: Any] {
+        return self + CandyAttribute.dictionary(from: [Attribute.font(font)])
+    }
+
+    public func withParagraphStyle(_ style: ParagraphStyle) -> [StringKey: Any] {
+        return self + CandyAttribute.dictionary(from: [Attribute.paragraphStyle(style)])
+    }
+
+    public func withBackgroundColor(_ color: Color) -> [StringKey: Any] {
+        return self + CandyAttribute.dictionary(from: [Attribute.backgroundColor(color)])
+    }
+
+    public func withLigatures(_ ligatures: Ligatures) -> [StringKey: Any] {
+        return self + CandyAttribute.dictionary(from: [Attribute.ligatures(ligatures)])
+    }
+
+    public func withKern(_ kernValue: Double) -> [StringKey: Any] {
+        return self + CandyAttribute.dictionary(from: [Attribute.kern(kernValue)])
+    }
+
+    public func withStrikethroughStyle(_ style: StrikethroughStyle) -> [StringKey: Any] {
+        return self + CandyAttribute.dictionary(from: [Attribute.strikethroughStyle(style)])
+    }
+
+    public func withUnderlineStyle(_ style: UnderlineStyle) -> [StringKey: Any] {
+        return self + CandyAttribute.dictionary(from: [Attribute.underlineStyle(style)])
+    }
+
+    public func withStrokeColor(_ color: Color) -> [StringKey: Any] {
+        return self + CandyAttribute.dictionary(from: [Attribute.strokeColor(color)])
+    }
+
+    public func withStrokeWidth(_ width: Double) -> [StringKey: Any] {
+        return self + CandyAttribute.dictionary(from: [Attribute.strokeWidth(width)])
+    }
+
+    public func withTextEffect(_ effect: TextEffect) -> [StringKey: Any] {
+        return self + CandyAttribute.dictionary(from: [Attribute.textEffect(effect)])
+    }
+
+    public func withBaselineOffset(_ offset: Double) -> [StringKey: Any] {
+        return self + CandyAttribute.dictionary(from: [Attribute.baselineOffset(offset)])
+    }
+
+    public func withUnderlineColor(_ color: Color) -> [StringKey: Any] {
+        return self + CandyAttribute.dictionary(from: [Attribute.underlineColor(color)])
+    }
+
+    public func withStrikethroughColor(_ color: Color) -> [StringKey: Any] {
+        return self + CandyAttribute.dictionary(from: [Attribute.strikethroughColor(color)])
+    }
+
+    public func withObliqueness(_ obliquenessValue: Double) -> [StringKey: Any] {
+        return self + CandyAttribute.dictionary(from: [Attribute.obliqueness(obliquenessValue)])
+    }
+
+    public func withExpansion(_ expansion: Double) -> [StringKey: Any] {
+        return self + CandyAttribute.dictionary(from: [Attribute.expansion(expansion)])
+    }
+
+    public func withWritingDirections(_ directions: [WritingDirection]) -> [StringKey: Any] {
+        return self + CandyAttribute.dictionary(from: [Attribute.writingDirections(directions)])
+    }
+
+    public func withVerticalGlyphForm(_ form: VerticalGlyphForm) -> [StringKey: Any] {
+        return self + CandyAttribute.dictionary(from: [Attribute.verticalGlyphForm(form)])
+    }
+
+    public func withShadow(_ shadow: Shadow) -> [StringKey: Any] {
+        return self + CandyAttribute.dictionary(from: [Attribute.shadow(shadow)])
+    }
+
+    public func withAttributes(_ attributes: [Attribute]) -> [String: Any] {
+        return self + CandyAttribute.dictionary(from: attributes)
+    }
+
+    public func withAttribute(_ attribute: Attribute) -> [String: Any] {
+        return self + CandyAttribute.dictionary(from: [attribute])
+    }
+}
+
